885d3adef58f19d4cdc117f7145ee7d98cf5d037
yana-sa <yanasamagala@gmail.com>
2021-01-21 17:38:38 +0200

    add recent books shortcode (reformated)

--- wp-content/plugins/test-plugin/test-plugin.php
+++ wp-content/plugins/test-plugin/test-plugin.php
@@ -9,8 +9,9 @@ Version: 1.0.0
 */
 
 //Book post type
-function create_books_post_type() {
-    register_post_type( 'books', [
+function create_books_post_type()
+{
+    register_post_type('books', [
             'labels' => [
                 'name' => 'Books',
                 'singular_name' => 'Book',
@@ -21,29 +22,33 @@ function create_books_post_type() {
             'public' => true,
             'has_archive' => true,
             'rewrite' => array('slug' => 'books'),
-            'capability_type'    => 'post',
+            'capability_type' => 'post',
             'show_in_rest' => true,
             'show_in_menu' => true,
             'taxonomies' => ['category', 'post_tag'],
-            'supports' => [ 'title', 'editor' ],
+            'supports' => ['title', 'editor'],
             'menu_position' => 5
         ]
     );
 }
-add_action( 'init', 'create_books_post_type' );
+
+add_action('init', 'create_books_post_type');
 
 //Book Category
-function insert_book_category() {
-    wp_insert_term( 'Book Category', 'category', [
+function insert_book_category()
+{
+    wp_insert_term('Book Category', 'category', [
             'description' => 'This is a book category',
             'slug' => 'book-category'
         ]
     );
 }
-add_action( 'after_setup_theme', 'insert_book_category' );
+
+add_action('after_setup_theme', 'insert_book_category');
 
 //Shortcode to show recently added books
-function fetch_books_shortcode(){
+function fetch_books_shortcode()
+{
 
     $query = new WP_Query([
         'post_type' => 'books',
@@ -53,7 +58,7 @@ function fetch_books_shortcode(){
     if ($query->have_posts()) {
         while ($query->have_posts()) {
             $query->the_post();
-            echo '<li><a href="'.get_permalink().'">' . get_the_title() . '</a></li>';
+            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
         }
     } else {
         echo 'Not found';
