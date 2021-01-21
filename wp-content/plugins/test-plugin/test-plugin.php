a4b636592cf588a63e2361beb4e45843e912931c
yana-sa <yanasamagala@gmail.com>
2021-01-21 17:11:19 +0200

    add shortcode (broken)

--- wp-content/plugins/test-plugin/test-plugin.php
+++ wp-content/plugins/test-plugin/test-plugin.php
@@ -7,6 +7,8 @@ Author: Unknown Yana
 Author URI: http://localhost:8000
 Version: 1.0.0
 */
+
+//Book post type
 function create_books_post_type() {
     register_post_type( 'books', [
             'labels' => [
@@ -30,11 +32,42 @@ function create_books_post_type() {
 }
 add_action( 'init', 'create_books_post_type' );
 
+//Book Category
 function insert_book_category() {
     wp_insert_term( 'Book Category', 'category', [
             'description' => 'This is a book category',
-            'slug'    => 'book-category'
+            'slug' => 'book-category'
         ]
     );
 }
 add_action( 'after_setup_theme', 'insert_book_category' );
+
+//Shortcode to show recently added books
+/*function fetch_books_shortcode() {
+
+    $message = 'Books will be displayed here';
+
+    return $message;
+}
+add_shortcode('fetched_books', 'fetch_books_shortcode');*/
+
+
+function fetch_books_shortcode(){
+
+    $query = new WP_Query([
+        'category_name' => 'book-category',
+        'posts_per_page' => 3
+    ]);
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            echo '<li>' . get_the_title() . '</li>';
+        }
+    } else {
+        echo 'Not found';
+    }
+    wp_reset_postdata();
+}
+
+add_shortcode('fetched_books', 'fetch_books_shortcode');
