5ad3bba18a32c38828054f1d34c3203d368fa174
yana-sa <yanasamagala@gmail.com>
2021-01-27 17:18:21 +0200

    jquery

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -10,16 +10,3 @@ function my_theme_scripts() {
     wp_enqueue_script( 'jquery-3.5.1', get_template_directory_uri() . '/js/jquery-3.5.1.js', array( 'jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
-
-function my_theme_enqueue_styles() {
-
-$parent_style = 'twentytwentyone';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
