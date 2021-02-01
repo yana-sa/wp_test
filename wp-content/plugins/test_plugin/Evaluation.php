1c41a177f39a7282df6c60136e3beebdec1f2d5c
yana-sa <yanasamagala@gmail.com>
2021-02-01 17:25:00 +0200

    book evaluation - OOP || private wpdb

--- wp-content/plugins/test_plugin/Evaluation.php
+++ wp-content/plugins/test_plugin/Evaluation.php
@@ -15,6 +15,7 @@ class Evaluation
         $user_id = get_current_user_id();
         $post_id = $_POST['post_id'];
         $evaluation = $_POST['evaluation'];
+        $table_name = $this->wpdb->prefix . 'book_evaluation';
 
         $rating = get_post_meta($post_id, '_rating_for_books', true);
         $error_message = $this->validate_book_evaluation($user_id, $post_id, $evaluation);
@@ -30,7 +31,7 @@ class Evaluation
         }
 
         $rating_update = update_post_meta($post_id, '_rating_for_books', $new_rating);
-        $this->wpdb->insert('wp_book_evaluation', ['user_id' => $user_id, 'post_id' => $post_id, 'action' => $evaluation], ['%s']);
+        $this->wpdb->insert($table_name, ['user_id' => $user_id, 'post_id' => $post_id, 'action' => $evaluation], ['%s']);
 
         if ($rating_update !== false) {
             $rating = $new_rating;
@@ -41,12 +42,13 @@ class Evaluation
 
     private function validate_book_evaluation($user_id, $post_id, $evaluation)
     {
+        $table_name = $this->wpdb->prefix . 'book_evaluation';
         if (!in_array($evaluation, ['like', 'dislike'])) {
             return 'Evaluation type is not valid';
         }
 
         if (!empty($user_id)) {
-            $is_eval = $this->wpdb->get_col("SELECT 1 FROM $this->wpdb->wp_book_evaluation WHERE user_id = '$user_id' AND post_id = '$post_id'", ARRAY_A);
+            $is_eval = $this->wpdb->get_col("SELECT 1 FROM $table_name WHERE user_id = '$user_id' AND post_id = '$post_id'", ARRAY_A);
             if (!empty($is_eval)) {
                 return 'You have already rated this post';
             }
