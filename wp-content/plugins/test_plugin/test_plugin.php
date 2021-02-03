3a4ef626ae184cccebe429f812784085db6a11d4
yana-sa <yanasamagala@gmail.com>
2021-02-03 16:24:37 +0200

    jquery likes || done

--- wp-content/plugins/test_plugin/test_plugin.php
+++ wp-content/plugins/test_plugin/test_plugin.php
@@ -137,8 +137,7 @@ function get_book_evaluation_action()
     global $wpdb;
     $post_id = get_the_ID();
     $user_id = get_current_user_id();
-    $sql = $wpdb->get_var("SELECT action FROM wp_book_evaluation WHERE user_id = '$user_id' AND post_id = '$post_id'");
-    return $sql;
+    return $wpdb->get_var("SELECT action FROM wp_book_evaluation WHERE user_id = '$user_id' AND post_id = '$post_id'");
 }
 function script_enqueue()
 {
