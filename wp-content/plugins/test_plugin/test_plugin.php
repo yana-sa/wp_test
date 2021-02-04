d8797f49b91ea99ca72d6d9958f2914e21ff1d61
yana-sa <yanasamagala@gmail.com>
2021-02-04 15:40:45 +0200

    book evaluation shortcode

--- wp-content/plugins/test_plugin/test_plugin.php
+++ wp-content/plugins/test_plugin/test_plugin.php
@@ -218,7 +218,11 @@ function fetch_books_shortcode()
         while ($query->have_posts()) {
             $query->the_post();
             $rating = get_post_meta(get_the_ID(), '_rating_for_books', true);
-            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a><br> Rating: <b>' . $rating . '</b></li>';
+            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a><br>
+                <p id="rating_for_books">Rating: ' . $rating . '</p>
+                <input type="button" formmethod="post" value="&#10133" id="like" data-button="evaluation" data-post-id="' . get_the_ID() . '" data-action="like"' . (get_book_evaluation_action() == 'like' ? "style='background-color:#8c8c8c' data-chosen='true'" : " ") . '>
+                <input type="button" formmethod="post" value="&#10134" id="dislike" data-button="evaluation" data-post-id="' . get_the_ID() . '" data-action="dislike"' . (get_book_evaluation_action() == 'dislike' ? "style='background-color:#8c8c8c' data-chosen='true'" : " ") . '>
+                </li>';
         }
     } else {
         echo 'Not found';
