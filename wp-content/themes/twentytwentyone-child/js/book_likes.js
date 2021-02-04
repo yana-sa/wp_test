49f8db582d90f9e2636e7899e5d35ef1523fe824
yana-sa <yanasamagala@gmail.com>
2021-02-04 10:59:56 +0200

    jquery likes - reset - not working

--- wp-content/themes/twentytwentyone-child/js/book_likes.js
+++ wp-content/themes/twentytwentyone-child/js/book_likes.js
@@ -1,58 +1,58 @@
 jQuery(document).ready( function() {
     jQuery('[data-button="evaluation"]').click( function(e) {
         e.preventDefault();
-        post_id = jQuery(this).attr('data-post-id');
-        evaluation = jQuery(this).attr('data-action');
-        jQuery.ajax({
-            type : "post",
-            dataType : "json",
-            url : myAjax.ajaxurl,
-            data : {action:"book_evaluation_data", post_id:post_id, evaluation:evaluation},
-            success: function(response) {
-                if(response.status === "success") {
-                    jQuery("#rating_for_books").html("Rating: "+ response.rating_for_books);
-                    if (evaluation === 'like') {
-                        jQuery("#like").attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
-                    }
-                    if (evaluation === 'dislike') {
-                        jQuery("#dislike").attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
+        if (jQuery(this).attr('[data-chosen]') === undefined) {
+            post_id = jQuery(this).attr('data-post-id');
+            evaluation = jQuery(this).attr('data-action');
+            jQuery.ajax({
+                type: "post",
+                dataType: "json",
+                url: myAjax.ajaxurl,
+                data: {action: "book_evaluation_data", post_id: post_id, evaluation: evaluation},
+                success: function (response) {
+                    if (response.status === "success") {
+                        jQuery("#rating_for_books").html("Rating: " + response.rating_for_books);
+                        if (evaluation === 'like') {
+                            jQuery("#like").attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
+                        }
+                        if (evaluation === 'dislike') {
+                            jQuery("#dislike").attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
+                        }
+                    } else {
+                        alert(response.message);
                     }
                 }
-                else {
-                    alert(response.message);
-                }
-            }
-        });
+            });
+        }
     });
 });
 
 jQuery(document).ready( function() {
     jQuery('[data-button="evaluation"]').click( function(e) {
         e.preventDefault();
-        if (jQuery(this).attr("data-chosen", "true")) {
+        if (jQuery(this).attr('[data-chosen]') !== undefined) {
             post_id = jQuery(this).attr('data-post-id');
             evaluation = jQuery(this).attr('data-action');
             jQuery.ajax({
-                type : "post",
-                dataType : "json",
-                url : myAjax.ajaxurl,
-                data : {action:"reset_book_evaluation", post_id:post_id, evaluation:evaluation},
-                success: function(response) {
-                    if(response.status === "success") {
+                type: "post",
+                dataType: "json",
+                url: myAjax.ajaxurl,
+                data: {action: "reset_book_evaluation", post_id: post_id, evaluation: evaluation},
+                success: function (response) {
+                    if (response.status === "success") {
                         if (evaluation === 'like') {
-                            jQuery("#like").attr("style", "background-color:#efefef").attr("data-chosen", "false");
-                            jQuery("#rating_for_books").html("Rating: "+ response.rating_for_books);
+                            jQuery("#like").attr("style", "background-color:#efefef").removeAttr("data-chosen");
+                            jQuery("#rating_for_books").html("Rating: " + response.rating_for_books);
                         }
                         if (evaluation === 'dislike') {
-                            jQuery("#dislike").attr("style", "background-color:#efefef").attr("data-chosen", "false");
-                            jQuery("#rating_for_books").html("Rating: "+ response.rating_for_books);
+                            jQuery("#dislike").attr("style", "background-color:#efefef").removeAttr("data-chosen");
+                            jQuery("#rating_for_books").html("Rating: " + response.rating_for_books);
                         }
-                    }
-                    else {
+                    } else {
                         alert(response.message);
                     }
                 }
             });
         }
-    })
+    });
 });
