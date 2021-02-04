5938607e2a02d213b8cde161e0ddc97664fcbf7c
yana-sa <yanasamagala@gmail.com>
2021-02-04 18:33:54 +0200

    book evaluation shortcode || done

--- wp-content/themes/twentytwentyone-child/js/book_likes.js
+++ wp-content/themes/twentytwentyone-child/js/book_likes.js
@@ -1,17 +1,18 @@
 jQuery(document).ready(function () {
     jQuery('[data-button="evaluation"]').click(function (e) {
         e.preventDefault();
-        post_id = jQuery(this).attr('data-post-id');
-        evaluation = jQuery(this).attr('data-action');
+        var post_id = jQuery(this).attr('data-post-id');
+        var evaluation = jQuery(this).attr('data-action');
+        var $this = jQuery(this)
         if (jQuery(this).attr('data-chosen') === undefined) {
-            sendEvaluationRequest(post_id, evaluation);
+            sendEvaluationRequest(post_id, evaluation, $this);
         } else {
-            sendResetEvaluationRequest(post_id, evaluation);
+            sendResetEvaluationRequest(post_id, evaluation, $this);
         }
     });
 });
 
-function sendEvaluationRequest(post_id, evaluation) {
+function sendEvaluationRequest(post_id, evaluation, $this) {
     jQuery.ajax({
         type: "post",
         dataType: "json",
@@ -19,12 +20,12 @@ function sendEvaluationRequest(post_id, evaluation) {
         data: {action: "book_evaluation_data", post_id: post_id, evaluation: evaluation},
         success: function (response) {
             if (response.status === "success") {
-                jQuery(this).closest('[data-parent="parent"]').find('[data-p="book-rating"]').html("Rating: " + response.rating_for_books);
+                $this.closest('[data_parent="parent"]').find('[data-p="book-rating"]').html("Rating: " + response.rating_for_books);
                 if (evaluation === 'like') {
-                    jQuery(this).attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
+                    $this.attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
                 }
                 if (evaluation === 'dislike') {
-                    jQuery(this).attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
+                    $this.attr("style", "background-color:#8c8c8c").attr("data-chosen", "true");
                 }
             } else {
                 alert(response.message);
@@ -33,7 +34,7 @@ function sendEvaluationRequest(post_id, evaluation) {
     });
 }
 
-function sendResetEvaluationRequest(post_id, evaluation) {
+function sendResetEvaluationRequest(post_id, evaluation, $this) {
     jQuery.ajax({
         type: "post",
         dataType: "json",
@@ -41,12 +42,12 @@ function sendResetEvaluationRequest(post_id, evaluation) {
         data: {action: "reset_book_evaluation", post_id: post_id},
         success: function (response) {
             if (response.status === "success") {
-                jQuery(this).closest('[data-parent="parent"]').find('[data-p="book-rating"]').html("Rating: " + response.rating_for_books);
+                $this.closest('[data_parent="parent"]').find('[data-p="book-rating"]').html("Rating: " + response.rating_for_books);
                 if (evaluation === 'like') {
-                    jQuery(this).attr("style", "background-color:#efefef").removeAttr("data-chosen");
+                    $this.attr("style", "background-color:#efefef").removeAttr("data-chosen");
                 }
                 if (evaluation === 'dislike') {
-                    jQuery(this).attr("style", "background-color:#efefef").removeAttr("data-chosen");
+                    $this.attr("style", "background-color:#efefef").removeAttr("data-chosen");
                 }
             } else {
                 alert(response.message);
